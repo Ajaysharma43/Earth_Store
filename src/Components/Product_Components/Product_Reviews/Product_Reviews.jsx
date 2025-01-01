514396cc253a57afb1ddafea25d120630b580302
ajay sharma <ajaysharma445446@gmail.com>
2025-01-01 18:23:30 +0530

    new changes made

--- src/Components/Product_Components/Product_Reviews/Product_Reviews.jsx
+++ src/Components/Product_Components/Product_Reviews/Product_Reviews.jsx
@@ -1,12 +1,46 @@
-const Product_Reviews = () => {
-    return(
+import { useState } from "react";
+
+export const DescriptionContent = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>This is the description content for the product.</p>
+    </div>
+  );
+};
+
+export const ReviewsContent = () => {
+    return (
         <>
-        <div className="flex justify-center gap-[20px] mr-[30%]">
-            <button><h1>description</h1></button>
-            <button>reviews</button>
-        </div>
+        <h1>this is reviews part</h1>
         </>
     )
 }
+
+const Product_Reviews = () => {
+  const [activeContent, setActiveContent] = useState("description");
+
+  const renderContent = () => {
+    if (activeContent === "description") {
+      return <DescriptionContent />;
+    } else if (activeContent === "reviews") {
+      return <ReviewsContent/>;
+    }
+  };
+
+  return (
+    <>
+      <div className="flex justify-center gap-[20px] mr-[30%]">
+        <button onClick={() => setActiveContent("description")}>
+          <h1>Description</h1>
+        </button>
+        <button onClick={() => setActiveContent("reviews")}>
+          <h1>Reviews</h1>
+        </button>
+      </div>
+      <div className="grid justify-center mr-[50%]">{renderContent()}</div>
+    </>
+  );
+};
 
 export default Product_Reviews;
