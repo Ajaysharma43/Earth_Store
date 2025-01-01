3b48b6eeec0d367a4dcdeb46143cf27fe4114cee
ajay sharma <ajaysharma445446@gmail.com>
2025-01-01 17:58:03 +0530

    new changes made

--- src/Components/Product_Components/Product_Reviews/Product_Reviews.jsx
+++ src/Components/Product_Components/Product_Reviews/Product_Reviews.jsx
@@ -1,7 +1,10 @@
 const Product_Reviews = () => {
     return(
         <>
-        
+        <div className="flex justify-center gap-[20px] mr-[30%]">
+            <button><h1>description</h1></button>
+            <button>reviews</button>
+        </div>
         </>
     )
 }
