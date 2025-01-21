c95238e04e55adc5a40553a9925ce86e27cfc79f
ajay sharma <ajaysharma445446@gmail.com>
2025-01-21 19:19:14 +0530

    changes

--- src/Components/Homepage_Components/Front_Page/Front_Page.jsx
+++ src/Components/Homepage_Components/Front_Page/Front_Page.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./Front_Page.css"
 
 const Front_Page = () => {
@@ -7,7 +8,9 @@ const Front_Page = () => {
         <div style={{height: "578px",marginTop: "0%"}} id="Front_Page">
             <h1 id="Front_Page_Title">EARTH</h1>
             <h1 id="Front_Page_Paragraph">MULTIPURPOSE STORE</h1>
+            <Link to={'/shop'}>
             <button id="Front_Page_Button">SHOP NOW</button>
+            </Link>
         </div>
         </center>
         </>
