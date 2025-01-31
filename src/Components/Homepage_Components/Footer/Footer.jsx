f3c13c542c8796c9e1f53de47a3901f1946d5482
ajay sharma <ajaysharma445446@gmail.com>
2025-01-31 17:52:17 +0530

    changes

--- src/Components/Homepage_Components/Footer/Footer.jsx
+++ src/Components/Homepage_Components/Footer/Footer.jsx
@@ -3,32 +3,31 @@ import Logout from "../../Logout/Logout";
 import "./Footer.css";
 
 const Footer = () => {
+  useEffect(() => {}, [Logout]);
 
-  useEffect(() => {
-
-  },[Logout])
   return (
-    <>
-      <div id="HomapageFooter">
-        <div>
-          <nav id="Homepage_Footer_Nav">
-            <h1 className="Homepage_Footer_Nav_Element">Home</h1>
-            <h1 className="Homepage_Footer_Nav_Element">About</h1>
-            <h1 className="Homepage_Footer_Nav_Element">Shop</h1>
-            <h1 className="Homepage_Footer_Nav_Element">Contact</h1>
-          </nav>
+    <div className="bg-white text-black py-8 m-4">
+      <div className="flex flex-col md:flex-row justify-between items-center">
+        <nav className="md:flex lg:flex xl:flex 2xl:flex lg:space-x-8 xl:space-x-8 2xl:space-x-8 ">
+          <h1 className="text-lg font-semibold hover:text-gray-400">Home</h1>
+          <h1 className="text-lg font-semibold hover:text-gray-400">About</h1>
+          <h1 className="text-lg font-semibold hover:text-gray-400">Shop</h1>
+          <h1 className="text-lg font-semibold hover:text-gray-400">Contact</h1>
+        </nav>
+
+        <div className="my-4 md:my-0">
+          <img
+            src="https://websitedemos.net/earth-store-02/wp-content/uploads/sites/1171/2022/11/EARTH-STORE-200x35.png"
+            alt="Earth Store Logo"
+            className="w-40"
+          />
         </div>
 
-        <div style={{    width: "15%",height:"0px",textAlign:"end"}}>
-            <img src="https://websitedemos.net/earth-store-02/wp-content/uploads/sites/1171/2022/11/EARTH-STORE-200x35.png" alt="" id="Homepage_Footer_Logo"/>
-
-        </div>
-
         <div>
-            <Logout/>
+          <Logout />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
